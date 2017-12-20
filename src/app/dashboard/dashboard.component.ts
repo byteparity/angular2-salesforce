e89b7cc39f860f3e4a05a34b17c9e5c7a0db322f
Gaurav Khambhala <gauravkhambhala@gmail.com>
2017-12-20 16:34:09 +0530

    Updated Angular version Added persistence login functionality

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../common/interfaces/contact';
+import { ForceService } from '../common/services';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  providers: [ForceService],
+})
+export class DashboardComponent implements OnInit {
+
+  contacts: any;
+  selectedContact: Contact;
+  loggedUser: any;
+
+  constructor(private forceService: ForceService) {
+    this.forceService.init({
+      appId: '3MVG9sG9Z3Q1Rlbc4tkIx2fI3ZUDVyYt86Ypl8ZqBXTpzPbQNHxq7gpwKcN75BB.fpgHxzSWgwgRY6nVfvBUe',
+      proxyURL: 'https://dev-cors-proxy.herokuapp.com/'
+    });
+  }
+
+  ngOnInit() {
+    if (this.forceService.isLoggedIn()) {
+      this.onLogin();
+    }
+  }
+
+  onLogin() {
+    this.forceService.login().then(() => {
+      this.loggedUser = this.forceService.getUserId();
+      this.getData();
+    });
+  }
+
+  getData() {
+    this.forceService.query('select id, firstname, lastname, phone from contact').then(result => this.contacts = (<any>result).records);
+  }
+
+  onSelect(contact: Contact) {
+      this.selectedContact = contact;
+  }
+
+}
